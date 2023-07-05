4a29985d6d29a6c8904cba5b15b69c0f3f4f6e29
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-05 14:35:24 +0900

    userモデルバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ validates :nickname,  presence: true
+ validates :sir_name_kanji, presence: true
+ validates :last_name_kanji, presence: true
+ VALID_NAME_REGEX =/[ぁ-んァ-ン一-龥々]/
+
+with_options presence: true do
+validates :sir_name_kanji, format: { with: VALID_NAME_REGEX, message: '全角で入力してください' }
+validates :last_name_kanji, format: { with: VALID_NAME_REGEX, message: '全角で入力してください' }
+end
+
+VALID_NAME_REGEX =/[ァ-ン]/
+
+with_options presence: true do
+validates :sir_name_kana, format: { with: VALID_NAME_REGEX, message: 'カタカナで入力してください' }
+validates :last_name_kana, format: { with: VALID_NAME_REGEX, message: 'カタカナで入力してください' }
+end
+
+ validates :birth_date, presence: true
+ VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ validates :password, format: { with: VALID_PASSWORD_REGEX, message: "半角英数を両方含む必要があります"}
+
+
 end
