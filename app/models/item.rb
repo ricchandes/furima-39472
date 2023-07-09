3a33d0283ac53a62739a6e4b74796052c54bff49
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-09 13:24:55 +0900

    itemモデルバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,10 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :status_id, presence: true
-  validates :delivery_price_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_date_id, presence: true
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_price_id,numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :price, presence: true
 
 
