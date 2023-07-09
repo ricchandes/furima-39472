934885df2975526ef2f4f890ba1500ab942e31e7
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-09 18:35:01 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,14 @@
 class Item < ApplicationRecord
   validates :name, presence: true
-  validates :image, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_price_id,numericality: { other_than: 1, message: "can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :price, presence: true
+  VALID_PRICE_REGEX = /\A[0-9]+\z
+  validates :price, format: { with: VALID_PRICE_REGEX, message: '半角数字で入力する必要があります' }
+  has_one_attached :image
 
 
   belongs_to :user
