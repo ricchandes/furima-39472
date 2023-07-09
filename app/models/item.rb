90933aaef79d00c6790da4d7b390d19bb69e3865
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-09 11:26:00 +0900

    itemモデルの設定(カラムの追加、バリデーション、アソシエーション)

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_price_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_date_id, presence: true
+  validates :price, presence: true
+
+
+  belongs_to :user
+
+end
