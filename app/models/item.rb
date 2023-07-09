656727996f60109004972b24dcca082b58b7d54b
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-09 12:35:57 +0900

    category_id アクティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :image, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :status_id, presence: true
   validates :delivery_price_id, presence: true
   validates :prefecture_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_014017) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_032517) do
+  create_table "categories", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "image", null: false
