6c174d620385200972d979a63ae08639761f826b
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-09 11:37:55 +0900

    itemsコントローラの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Items.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private #ここからプライベートだよー
+  def item_params
+    params.require(:item).permit(:name, :explanation, :image, :category_id, :status_id, :delivery_price_id, :delivery_price_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
+
+
+  
 end
