3d6c00b134a0f7d27751dc0aac5274c234639557
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-11 15:35:22 +0900

    itemモデルvalidation修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,16 @@
 class Item < ApplicationRecord
   validates :name, presence: true
+  validates :image, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_price_id,numericality: { other_than: 1, message: "can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" } 
-
   with_options presence: true, format: {with: /\A[0-9]+\z/} do
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
        presence: {message:  "can't be blank"}
-    end
-  
-  
-
-  has_one_attached :image
+  end
   belongs_to :user
   
   extend ActiveHash::Associations::ActiveRecordExtensions
