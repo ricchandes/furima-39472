cf8b7cc86d38cc37e41a32dcbf4bbf09b3093fcb
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-12 21:08:59 +0900

    destroyアクションの定義(コントローラー)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
+
   private # ここからプライベートだよー
 
   def item_params
