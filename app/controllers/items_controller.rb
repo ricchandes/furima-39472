314cd5eba467171e9b9302c42a7e3f8a4749151d
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-12 17:59:26 +0900

    showアクションの定義(コントローラー)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   def new
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private #ここからプライベートだよー
