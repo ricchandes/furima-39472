6f71f6fda9d09e2eedad3ccca6b7fd8a88007f82
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-13 15:41:38 +0900

    カラムの入力(order,addressマイグレーション)

--- db/migrate/20230713054328_create_addresses.rb
+++ db/migrate/20230713054328_create_addresses.rb
@@ -1,8 +1,16 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-
-      t.timestamps
+    t.string   :postcode,               null: false
+    t.integer  :prefecture_id,          null: false
+    t.string   :city,                   null: false
+    t.string   :address,                null: false
+    t.string   :building,                null: false
+    t.string   :tell,                    null: false
+    t.references :order,    null: false, foreign_key: true  
+    t.timestamps
     end
   end
 end
+
+
