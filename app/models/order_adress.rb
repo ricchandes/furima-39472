b888389b7221c7386f292f5294ec674c6cd8708a
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-13 17:31:50 +0900

    formオブジェクトの設計

--- app/models/order_adress.rb
+++ app/models/order_adress.rb
@@ -1,4 +1,24 @@
 class OrderAdress < ApplicationRecord
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :price, :user_id
+  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :tell, :user_id, :order_id, :address_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :order_id
+    validates :address_id
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :tell, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Exclude hyphen(-)"}
+    validates :city
+    validates :address
+
+
+  end
+
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(order_id: order_id, user_id: user_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building: building, tell: tell, order: order)
+  end
+
 end
