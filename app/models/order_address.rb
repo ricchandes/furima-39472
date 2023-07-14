555e7c40faf02d9ae5d2379428fcc889cdf30d2e
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-14 18:20:48 +0900

    オブジェクトフォームの編集

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,26 +1,21 @@
 class OrderAddress < ApplicationRecord
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :tell, :user_id, :order_id, :address_id, :token
+  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :tell, :token, :item_id, :order_id, :user_id
 
   with_options presence: true do
     validates :user_id
-    validates :order_id
-    validates :address_id
+    validates :address
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :tell, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Exclude hyphen(-)"}
     validates :city
-    validates :address
-    validates :token
-
-
-
   end
 
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    order = Order.create(order_id: order_id, user_id: user_id)
-    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building: building, tell: tell, order: order)
+    order = Order.create(item_id: item_id, user_id: user_id)
+
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building: building, tell: tell, order_id: order.id)
   end
 
 
