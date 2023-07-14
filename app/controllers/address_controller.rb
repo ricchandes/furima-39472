2973047a585bebc171ee1c2b69dbddaed2622f3e
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-14 17:30:19 +0900

    addressコントローラアクション定義いらなかった。

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,22 +1,3 @@
 class AddressController < ApplicationController
-  def new
-    @order_address = OrderAddress.new
-  end
-
-  def create
-    @order_address = OrderAddress.new(address_params)
-    if @order_address.valid?
-      @order_address.save
-      redirect_to root_path
-    else
-      render :index, status: :unprocessable_entity  
-    end
-  end
-
-  private
-
-  def address_params
-    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :address, :building, :tell).merge(user_id: current_user.id)
-  end
 
 end
