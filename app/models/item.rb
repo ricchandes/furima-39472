0a459f2b0e1b1eece46f8d9a34e59cfc3ad92525
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-20 18:25:02 +0900

    ビューへの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,11 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_one :order
   has_many :likes
-  
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
