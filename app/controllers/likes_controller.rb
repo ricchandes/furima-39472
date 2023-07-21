9670e83802bc59398408933ba79f6f642b6f2ebe
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-21 18:02:39 +0900

    いいねの実装(コントローラー)

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,27 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  def create
+    like = current_user.likes.build(item_id: params[:item_id])
+    like.save
+    binding.pry
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def destroy
+    like = Like.find_by(params[:item_id])
+    like.destroy
+    respond_to do |format|
+      format.js
+    end
+
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
