5cb69757001d046c83aec8ca84a24d3990b3084d
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-21 18:47:44 +0900

    コメント(モデル、DB)の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_083134) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_093932) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_083134) do
   end
 
   create_table "categories", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "text"
+    t.integer "user_id"
+    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
